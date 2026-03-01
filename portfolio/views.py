02026d5b596a51288272ef8e099f25ba885f2560
oniichan-cyber <sangmagam35@gmail.com>
2026-03-01 12:16:11 +0530

    First commit

---
+++ portfolio/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'portfolio/home.html')
+
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+def contact(request):
+    return render(request, 'portfolio/contact.html')
